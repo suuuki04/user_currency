f983995ac06fce35c5adb9e2305d923a7717d726
suki <astre15@naver.com>
2024-11-29 21:14:29 +0900

    Update: 예외처리 추가
    1.예외처리를 추가했습니다.

--- src/main/java/com/sparta/currency_user/dto/ExchangeRequestDto.java
+++ src/main/java/com/sparta/currency_user/dto/ExchangeRequestDto.java
@@ -1,16 +1,21 @@
 package com.sparta.currency_user.dto;
 
 import lombok.Getter;
+import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
+
 
 @Getter
 public class ExchangeRequestDto {
 
+    @NotNull(message = "고객 아이디는 필수입니다.")
     private Long userId;
 
+    @NotNull(message = "금액을 입력해야 합니다.")
     private BigDecimal amountInKrw;
 
+    @NotNull(message = "통화 아이디는 필수입니다.")
     private Long currencyId;
 
     public ExchangeRequestDto(Long userId, BigDecimal amountInKrw, Long currencyId) {
