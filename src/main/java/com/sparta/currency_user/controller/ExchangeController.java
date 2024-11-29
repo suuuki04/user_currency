881b45cd2263ac14de12c5c7a78bb7b810639660
suki <astre15@naver.com>
2024-11-29 18:06:34 +0900

    Update: 특정고객이 수행한 환전요청 조회 기능 생성
    1. 특정고객이 수행한 환전 요청 조회

---
+++ src/main/java/com/sparta/currency_user/controller/ExchangeController.java
@@ -0,0 +1,34 @@
+package com.sparta.currency_user.controller;
+
+import com.sparta.currency_user.dto.ExchangeRequestDto;
+import com.sparta.currency_user.dto.ExchangeResponseDto;
+import com.sparta.currency_user.service.ExchangeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/exchanges")
+@RequiredArgsConstructor
+public class ExchangeController {
+
+    private final ExchangeService exchangeService;
+
+    //C:환전 요청 수행
+    @PostMapping
+    public ResponseEntity<ExchangeResponseDto> exchange(@RequestBody ExchangeRequestDto exchangeRequestDto) {
+        ExchangeResponseDto responseDto = exchangeService.exchange(exchangeRequestDto.getCurrencyId(), exchangeRequestDto);
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
+    }
+
+    //R: 특정 고객 이 수행한 환전 요청 조회
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<List<ExchangeResponseDto>> getExchangesByUserId(@PathVariable Long userId) {
+        List<ExchangeResponseDto> exchangeResponseDtos = exchangeService.getExchangesByUserId(userId);
+        return new ResponseEntity<>(exchangeResponseDtos, HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/sparta/currency_user/repository/ExchangRepository.java
@@ -0,0 +1,11 @@
+package com.sparta.currency_user.repository;
+
+import com.sparta.currency_user.entity.Exchange;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ExchangRepository extends JpaRepository<Exchange, Long> {
+
+    List<Exchange> findByUserId(Long userId);
+}
